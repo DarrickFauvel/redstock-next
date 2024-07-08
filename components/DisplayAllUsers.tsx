64dd8831489ae74904f5a35ac1e922b2ed51bc39
[Darrick Fauvel] <[darrick@darrickdevelops.com]>
2024-07-08 10:34:01 -0400

    Display table rows

--- components/DisplayAllUsers.tsx
+++ components/DisplayAllUsers.tsx
@@ -1,9 +1,35 @@
 import { getAllUsers } from "@/app/actions/user"
+import { Allura } from "next/font/google"
 
 const DisplayAllUsers = async () => {
   const allUsers = await getAllUsers()
-  // console.log(allUsers)
 
-  return <div className="mt-8">DisplayAllUsers</div>
+  const tableRowsOfUsers = allUsers.map((user) => {
+    return (
+      <tr key={user.id}>
+        <th>{user.id}</th>
+        <td>{user.name}</td>
+        <td>{user.email}</td>
+      </tr>
+    )
+  })
+
+  return (
+    <div className="mt-8">
+      DisplayAllUsers
+      <div className="overflow-x-auto">
+        <table className="table">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Email</th>
+            </tr>
+          </thead>
+          <tbody>{tableRowsOfUsers}</tbody>
+        </table>
+      </div>
+    </div>
+  )
 }
 export default DisplayAllUsers
